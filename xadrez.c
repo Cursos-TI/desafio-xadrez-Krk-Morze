9d428eb9995ab91202408363ba1b0f4e0ba9cbe9
Elias <eliasrubens333@gmail.com>
2025-09-12 01:45:51 -0300

    Update xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,21 +1,83 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+int main(){
+    int option;
+    int num;
+    char linha[50];
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    do {
+        printf("Menu do jogo de xadrez:\n");
+        printf("1. bispo\n");
+        printf("2. torre\n");
+        printf("3. rainha\n");
+        printf("4. sair\n");
+        printf("escolhar uma opçao: ");
+        scanf("%d", &option);
+       
+        switch(option) {
+            case 1:
+                do {
+                 printf("o bispo so pode se mover em 5 casas: ");
+                 scanf("%d", &num);
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+                 printf("escrevar uma das opçoes\n");
+                 printf("diagonal ou anti-diagonal\n");
+                 scanf("%s", &linha);
+       
+        if (num >= 5) {
+            printf("Você digitou: %d\n", num);
+            printf("bispo: %d casas na %s\n", num, linha);
+                
+        }
+    } while (num <= 5);
+   
+    printf("movimento impossivel. Saindo...\n");
+   
+                break;
+            case 2:
+                do {
+                 printf("torre so pode se mover em 5 casas: ");
+                 scanf("%d", &num);
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+                 printf("escrevar uma das opçoes\n");
+                 printf("direita, esquerda , cima ou baixo\n");
+                 scanf("%s", &linha);
+       
+        if (num >= 5) {
+            printf("Você digitou: %d\n", num);
+            printf("torre: %d casas na %s\n", num, linha);
+                
+        }
+    } while (num <= 5);
+   
+    printf("movimento impossivel. Saindo...\n");
+                break;
+            case 3:
+                do {
+                 printf("rainha so pode se mover em 8 casas: ");
+                 scanf("%d", &num);
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+                 printf("escrevar uma das opçoes\n");
+                 printf("qualquer direçao\n");
+                 scanf("%s", &linha);
+       
+        if (num >= 8) {
+            printf("Você digitou: %d\n", num);
+            printf("rainha: %d casas na %s\n", num, linha);
+                
+        }
+    } while (num <= 8);
+   
+    printf("movimento impossivel. Saindo...\n");
+                break;
+            case 4:
+                printf("Saindo...\n");
+                break;
+            default:
+                printf("Opção inválida!\n");
+        }
+    } while (option != 4);
+}
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
